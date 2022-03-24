58cefe124642f9bdaab65b9d40735d40b785751d
BlackParure <blackparurers@gmail.com>
2022-03-24 10:51:20 -0400

    First Commit

---
+++ My First Android Application.md
@@ -0,0 +1,10 @@
+## My First Android Application
+
+This is a repository for fresher to Android development, I will realize some foundation function from a simple frame work to a mobile computing application based on Snapdragon Neural Processing Engine SDK.
+
+### 1. Building Application Framework
+
+### 2. Pain Text, Text View and Button
+
+### 3. Button Event and Message Passing
+
---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /** Called when the user taps the Send button */
+    public void sendMessageToNewPage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+    /** Called when the user taps the Send button */
+    public void sendMessageToCurrentPage(View view) {
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        TextView textView = findViewById(R.id.textViewOutput);
+        String message = editText.getText().toString();
+        textView.setText(message);
+
+    }
+}
